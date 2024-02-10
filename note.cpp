9ac72f21f2621aca9359ecce4b36abf4777a1597
Semachko <vakdanek@gmail.com>
2024-02-10 18:10:46 +0200

    Setted size of notes

--- note.cpp
+++ note.cpp
@@ -2,13 +2,13 @@
 
 Note::Note(const QString title, const QString text, QWidget *parent) : QWidget(parent)
 {
-    //QWidget* widget = new QWidget();
     m_title = new QLabel(title);
     m_text = new QTextBrowser(this);
     m_text->setPlainText(text);
+
     QVBoxLayout* layout = new QVBoxLayout(this);
     layout->addWidget(m_title);
     layout->addWidget(m_text);
-    setMaximumSize(400,400);
-    setMinimumSize(200,200);
+    setMaximumSize(300,400);
+    setMinimumSize(200,300);
 }
